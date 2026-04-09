539ea6459f9a9a1de908fecbcae7965c890e0222
Coop <jcooper920@gmail.com>
2026-04-09 11:03:33 -0400

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://Coop920.github.io',
+  base: '/Vepoloxamer/', // remove this line for a user/org site repo
   server: { host: true }
 });
